a7f29e4bf13e4eac144db96228f2a325cdde240e
Shailesh Yadav <shailesh772002@gmail.com>
2026-09-04 11:55:22 +0530

    Project structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shailesh",
+    author_email="shailesh772002@gmail.com",
+    packages=find_packages()
+)
